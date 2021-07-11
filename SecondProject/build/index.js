aece2cd3579ba9ba76438961f2d84adc2c0a4bb5
Krystian Kluba <klubakrystian@gmail.com>
2021-07-11 12:00:23 +0200

    Nowy projekt

--- SecondProject/build/index.js
+++ SecondProject/build/index.js
@@ -1,105 +1,110 @@
-var channel1 = [];
-var clapAudio = document.querySelector('[data-sound="clap"]');
-var boomAudio = document.querySelector('[data-sound="boom"]');
-var hihatAudio = document.querySelector('[data-sound="hihat"]');
-var kickAudio = document.querySelector('[data-sound="kick"]');
-var openhatAudio = document.querySelector('[data-sound="openhat"]');
-var rideAudio = document.querySelector('[data-sound="ride"]');
-var snareAudio = document.querySelector('[data-sound="snare"]');
-var tinkAudio = document.querySelector('[data-sound="tink"]');
-var tomAudio = document.querySelector('[data-sound="tom"]');
-var playChannel1Btn = document.querySelector('#playChannel1');
-var playChannel1BtnRecorder = document.querySelector('#playChannel1Record');
-var progressBar = document.getElementById('prog-bar');
-var recorder = false;
-var differenceTime = 0;
-document.body.addEventListener('keypress', onKeyDown);
-playChannel1Btn.addEventListener('click', onPlayChannel1);
-playChannel1BtnRecorder.addEventListener('click', playChannel1Recorder);
-function playChannel1Recorder(ev) {
-    recorder = !recorder;
-    if (recorder) {
-        playChannel1BtnRecorder.innerHTML = "Stop";
-        differenceTime = ev.timeStamp;
-        channel1 = [];
-    }
-    else
-        playChannel1BtnRecorder.innerHTML = "Record";
-}
-function onKeyDown(ev) {
-    if (recorder) {
-        var key = ev.key;
-        var time = ev.timeStamp - differenceTime;
-        channel1.push({
-            key: key,
-            time: time
-        });
-        playSound(key, 0);
-        console.log(channel1);
-    }
-    else {
-        playSound(ev.key, 0);
-    }
-}
-function playSound(key, soundTime) {
-    progressBar.value = soundTime;
-    switch (key) {
-        case 'q':
-        case 'Q':
-            clapAudio.currentTime = 0;
-            clapAudio.play();
-            break;
-        case 'w':
-        case 'W':
-            boomAudio.currentTime = 0;
-            boomAudio.play();
-            break;
-        case 'e':
-        case 'E':
-            hihatAudio.currentTime = 0;
-            hihatAudio.play();
-            break;
-        case 'a':
-        case 'A':
-            kickAudio.currentTime = 0;
-            kickAudio.play();
-            break;
-        case 's':
-        case 'S':
-            openhatAudio.currentTime = 0;
-            openhatAudio.play();
-            break;
-        case 'd':
-        case 'D':
-            rideAudio.currentTime = 0;
-            rideAudio.play();
-            break;
-        case 'z':
-        case 'z':
-            snareAudio.currentTime = 0;
-            snareAudio.play();
-            break;
-        case 'x':
-        case 'X':
-            tinkAudio.currentTime = 0;
-            tinkAudio.play();
-            break;
-        case 'c':
-        case 'C':
-            tomAudio.currentTime = 0;
-            tomAudio.play();
-            break;
-    }
-}
-function onPlayChannel1() {
-    playChannel1();
-}
-function playChannel1() {
-    var prevTime = 0;
-    progressBar.max = channel1[channel1.length - 1].time;
-    progressBar.value = 0;
-    channel1.forEach(function (sound) {
-        var timeout = sound.time - prevTime;
-        setTimeout(function () { return playSound(sound.key, sound.time); }, timeout);
-    });
-}
+const clapAudioSound = document.querySelector(".clap");
+const boomAudioSound = document.querySelector(".boom");
+const hihatAudioSound = document.querySelector(".hihat");
+const kickAudioSound = document.querySelector(".kick");
+const openhatAudioSound = document.querySelector(".openhat");
+const rideAudioSound = document.querySelector(".ride");
+const snareAudioSound = document.querySelector(".snare");
+const tinkAudioSound = document.querySelector(".tink");
+const tomAudioSound = document.querySelector(".tom");
+
+const clapBtn = document.querySelector(".clapBtn");
+const boomBtn = document.querySelector(".boomBtn");
+const hihatBtn = document.querySelector(".hihatBtn");
+const kickBtn = document.querySelector(".kickBtn");
+const openhatBtn = document.querySelector(".openhatBtn");
+const rideBtn = document.querySelector(".rideBtn");
+const snareBtn = document.querySelector(".snareBtn");
+const tinkBtn = document.querySelector(".tinkBtn");
+const tomBtn = document.querySelector(".tomBtn");
+console.log(clapAudioSound);
+
+const clapAudioPlay = () => {
+  clapAudioSound.play();
+};
+
+const boomAudioPlay = () => {
+  boomAudioSound.play();
+};
+
+const hihatAudioPlay = () => {
+  hihatAudioSound.play();
+};
+
+
+const kickAudioPlay = () => {
+  kickAudioSound.play();
+};
+
+
+const openhatAudioPlay = () => {
+  openhatAudioSound.play();
+};
+
+
+const rideAudioPlay = () => {
+  rideAudioSound.play();
+};
+
+
+const snareAudioPlay = () => {
+  snareAudioSound.play();
+};
+
+const tinkAudioPlay = () => {
+  tinkAudioSound.play();
+};
+
+const tomAudioPlay = () => {
+  tomAudioSound.play();
+};
+
+
+const handleAudioPlayOnKeyPress = (e) => {
+  console.log(e.key);
+
+  switch (e.key) {
+    case "q":
+      clapAudioSound.play();
+      break;
+    case "w":
+      boomAudioSound.play();
+      break;
+    case "e":
+      hihatAudioSound.play();
+      break;
+    case "a":
+      kickAudioSound.play();
+      break;
+    case "s":
+      openhatAudioSound.play();
+      break;
+    case "d":
+      rideAudioSound.play();
+      break;
+    case "z":
+      snareAudioSound.play();
+      break;
+    case "x":
+      tinkAudioSound.play();
+      break;
+    case "c":
+      tomAudioSound.play();
+      break;
+
+    default:
+      break;
+  }
+};
+
+clapBtn.addEventListener("click", clapAudioPlay);
+boomBtn.addEventListener("click", boomAudioPlay);
+hihatBtn.addEventListener("click", hihatAudioPlay);
+kickBtn.addEventListener("click", kickAudioPlay);
+openhatBtn.addEventListener("click", openhatAudioPlay);
+rideBtn.addEventListener("click", rideAudioPlay);
+snareBtn.addEventListener("click", snareAudioPlay);
+tinkBtn.addEventListener("click", tinkAudioPlay);
+tomBtn.addEventListener("click", tomAudioPlay);
+
+document.body.addEventListener("keypress", handleAudioPlayOnKeyPress);
--- SecondProject/src/index.ts
+++ SecondProject/src/index.ts
@@ -1,119 +1,59 @@
-let channel1: any[] = [];
-const clapAudio: HTMLAudioElement = document.querySelector('[data-sound="clap"]');
-const boomAudio: HTMLAudioElement = document.querySelector('[data-sound="boom"]');
-const hihatAudio: HTMLAudioElement = document.querySelector('[data-sound="hihat"]');
-const kickAudio: HTMLAudioElement = document.querySelector('[data-sound="kick"]');
-const openhatAudio: HTMLAudioElement = document.querySelector('[data-sound="openhat"]');
-const rideAudio: HTMLAudioElement = document.querySelector('[data-sound="ride"]');
-const snareAudio: HTMLAudioElement = document.querySelector('[data-sound="snare"]');
-const tinkAudio: HTMLAudioElement = document.querySelector('[data-sound="tink"]');
-const tomAudio: HTMLAudioElement = document.querySelector('[data-sound="tom"]');
+const clapAudioSound:HTMLAudioElement = document.querySelector(".clap");
+const boomAudioSound:HTMLAudioElement = document.querySelector(".boom");
+const hihatAudioSound:HTMLAudioElement = document.querySelector(".hihat");
+const kickAudioSound:HTMLAudioElement = document.querySelector(".kick");
+const openhatAudioSound:HTMLAudioElement = document.querySelector(".openhat");
+const rideAudioSound:HTMLAudioElement = document.querySelector(".ride");
+const snareAudioSound:HTMLAudioElement = document.querySelector(".snare");
+const tinkAudioSound:HTMLAudioElement = document.querySelector(".tink");
+const tomAudioSound:HTMLAudioElement = document.querySelector(".tom");
 
-const playChannel1Btn: HTMLButtonElement = document.querySelector('#playChannel1');
-const playChannel1BtnRecorder: HTMLButtonElement = document.querySelector('#playChannel1Record');
-const progressBar: HTMLProgressElement = document.getElementById('prog-bar') as HTMLProgressElement;
+const clapBtn: HTMLButtonElement = document.querySelector(".clapBtn");
+console.log(clapAudioSound);
 
-let recorder: boolean = false;
-let differenceTime: number = 0;
-
-document.body.addEventListener('keypress', onKeyDown);
-playChannel1Btn.addEventListener('click', onPlayChannel1);
-playChannel1BtnRecorder.addEventListener('click', playChannel1Recorder);
-
-function playChannel1Recorder(ev: MouseEvent) {
-    recorder = !recorder;
-    if (recorder) {
-        playChannel1BtnRecorder.innerHTML = "Stop";
-        differenceTime = ev.timeStamp;
-        channel1 = [];
-    }
-    else playChannel1BtnRecorder.innerHTML = "Record";
-}
-
-function onKeyDown(ev: KeyboardEvent): void {
-    if (recorder) {
-        const key = ev.key;
-        const time = ev.timeStamp - differenceTime;
-
-        channel1.push({
-            key,
-            time
-        });
-
-        playSound(key, 0);
-        console.log(channel1)
-    }
-    else {
-        playSound(ev.key, 0);
-    }
-}
-
-function playSound(key: string, soundTime: any) {
-    progressBar.value = soundTime;
-    switch (key) {
-        case 'q':
-        case 'Q':
-            clapAudio.currentTime = 0;
-            clapAudio.play();
-            break;
-        case 'w':
-        case 'W':
-            boomAudio.currentTime = 0;
-            boomAudio.play();
-            break;
-        case 'e':
-        case 'E':
-            hihatAudio.currentTime = 0;
-            hihatAudio.play();
-            break;
-        case 'a':
-        case 'A':
-            kickAudio.currentTime = 0;
-            kickAudio.play();
-            break;
-        case 's':
-        case 'S':
-            openhatAudio.currentTime = 0;
-            openhatAudio.play();
-            break;
-        case 'd':
-        case 'D':
-            rideAudio.currentTime = 0;
-            rideAudio.play();
-            break;
-        case 'z':
-        case 'z':
-            snareAudio.currentTime = 0;
-            snareAudio.play();
-            break;
-        case 'x':
-        case 'X':
-            tinkAudio.currentTime = 0;
-            tinkAudio.play();
-            break;
-        case 'c':
-        case 'C':
-            tomAudio.currentTime = 0;
-            tomAudio.play();
-            break;
-    }
-}
-
-function onPlayChannel1(): void {
-    playChannel1();
-}
-
-function playChannel1(): void {
-    let prevTime = 0;
-    progressBar.max = channel1[channel1.length - 1].time;
-    progressBar.value = 0;
-
-    channel1.forEach(sound => {
-        const timeout = sound.time - prevTime;
-        setTimeout(() => playSound(sound.key, sound.time), timeout);
-
-    });
-}
+const handleAudioPlay = (): void => {
+  clapAudioSound.play();
+  boomAudioSound.play();
+};
 
 
 
+const handleAudioPlayOnKeyPress = (e: KeyboardEvent):void => {
+
+  switch (e.key) {
+    case "q":
+      clapAudioSound.play();
+      break;
+    case "w":
+      boomAudioSound.play();
+      break;
+    case "e":
+      hihatAudioSound.play();
+      break;
+    case "a":
+      kickAudioSound.play();
+      break;
+    case "s":
+      openhatAudioSound.play();
+      break;
+    case "d":
+      rideAudioSound.play();
+      break;
+    case "z":
+      snareAudioSound.play();
+      break;
+    case "x":
+      tinkAudioSound.play();
+      break;
+    case "c":
+      tomAudioSound.play();
+      break;
+
+    default:
+      break;
+  }
+};
+
+clapBtn.addEventListener("click", handleAudioPlay);
+
+document.body.addEventListener("keypress", handleAudioPlayOnKeyPress);
